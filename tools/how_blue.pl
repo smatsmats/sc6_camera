3f3a161431af7277fe9a28720a5687fa0d378c6d
John Smith <willey@selby.com>
2013-07-26 15:42:20 -0700

    fix some tools

--- tools/how_blue.pl
+++ tools/how_blue.pl
@@ -14,7 +14,8 @@ my $mode = "prod";
 my $dryrun = 0;
 my $sleep_time = 30;
 
-my $config = get_config($mode, $debug);
+my $c = new SC6::Cam::Config("/usr/local/cam/conf/config.yml");
+our $config = $c->getConfig();
 
 print $ARGV[0], "\n";
 get_blue($ARGV[0], 1);
