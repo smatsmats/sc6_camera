ba56530084b49f5af02c26ec17b11b2815038683
John Smith <willey@selby.com>
2015-04-13 12:07:55 -0700

    playing around with oauth2

---
+++ bin/util/get_authorization_code.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $oauth_url = "https://accounts.google.com/o/oauth2/auth";
+my $scope = "email%20profile";
+my $state = <state;
+my $redirect_uri = "urn:ietf:wg:oauth:2.0:oob";
+my $response_type = "code"; 
+my $client_id = <client_id>
+my $access_type = "offline"; 
+
+
+
+my $url = $oauth_url . "?" . 
+          "scope=" . $scope . "&" . 
+	  "state=" . $state . "&" . 
+	  "redirect_uri=" . $redirect_uri . "&" . 
+	  "response_type=" . $response_type . "&" . 
+	  "client_id=" . $client_id . "&" .
+	  "access_type=" . $access_type;
+
+my $cmd = "w3m '$url'";
+print $cmd, "\n";
+
+print "w3m -post post_file https://www.googleapis.com/oauth2/v3/token\n"
