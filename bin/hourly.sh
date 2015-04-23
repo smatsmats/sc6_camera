2df374c3446084ee88a507be7f6e36e8aaa72231
John Smith <willey@selby.com>
2015-04-22 20:38:06 -0700

    removed verbose debugging

--- bin/hourly.sh
+++ bin/hourly.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #time /usr/local/cam/bin/build_time_lapse.pl 
-time trickle -sv -u 200 /usr/local/cam/bin/build_time_lapse.pl $@
+time trickle -s -u 200 /usr/local/cam/bin/build_time_lapse.pl $@
