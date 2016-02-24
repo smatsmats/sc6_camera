d885f27ca4a133d64a7d4ef7d2d6f1af645c604e
John Smith <willey@selby.com>
2016-02-24 08:04:58 -0800

    seperate installer for libs

--- install.sh
+++ install.sh
@@ -5,15 +5,9 @@ sudo cp etc/default/cam_collect_images /etc/default
 
 [ -d /usr/local/lib/site_perl/SC6/Cam/ ] || sudo mkdir -p /usr/local/lib/site_perl/SC6/Cam
 
-sudo cp lib/site_perl/SC6/Cam/BlueCode.pm  /usr/local/lib/site_perl/SC6/Cam
-sudo cp lib/site_perl/SC6/Cam/BlueCodeState.pm  /usr/local/lib/site_perl/SC6/Cam
-sudo cp lib/site_perl/SC6/Cam/Config.pm  /usr/local/lib/site_perl/SC6/Cam
-sudo cp lib/site_perl/SC6/Cam/General.pm  /usr/local/lib/site_perl/SC6/Cam
-sudo cp lib/site_perl/SC6/Cam/GStore.pm  /usr/local/lib/site_perl/SC6/Cam
-sudo cp lib/site_perl/SC6/Cam/Image.pm  /usr/local/lib/site_perl/SC6/Cam
-sudo cp lib/site_perl/SC6/Cam/Overlay.pm  /usr/local/lib/site_perl/SC6/Cam
-sudo cp lib/site_perl/SC6/Cam/Sun.pm  /usr/local/lib/site_perl/SC6/Cam
-sudo cp lib/site_perl/SC6/Cam/WX.pm  /usr/local/lib/site_perl/SC6/Cam
+./install_libs.sh
+
+cp www/index.cgi /home/willey/sc6_camera_www
 
 crontab -l > /tmp/old_cron
 cat cron/crontab /tmp/old_cron | crontab -
---
+++ install_libs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo cp -p lib/site_perl/SC6/Cam/BlueCode.pm  /usr/local/lib/site_perl/SC6/Cam
+sudo cp -p lib/site_perl/SC6/Cam/BlueCodeState.pm  /usr/local/lib/site_perl/SC6/Cam
+sudo cp -p lib/site_perl/SC6/Cam/Config.pm  /usr/local/lib/site_perl/SC6/Cam
+sudo cp -p lib/site_perl/SC6/Cam/General.pm  /usr/local/lib/site_perl/SC6/Cam
+sudo cp -p lib/site_perl/SC6/Cam/GStore.pm  /usr/local/lib/site_perl/SC6/Cam
+sudo cp -p lib/site_perl/SC6/Cam/Image.pm  /usr/local/lib/site_perl/SC6/Cam
+sudo cp -p lib/site_perl/SC6/Cam/Overlay.pm  /usr/local/lib/site_perl/SC6/Cam
+sudo cp -p lib/site_perl/SC6/Cam/Sun.pm  /usr/local/lib/site_perl/SC6/Cam
+sudo cp -p lib/site_perl/SC6/Cam/WX.pm  /usr/local/lib/site_perl/SC6/Cam
+
