a4b2ab9e5a74d4e67b9fb6b91b356c618cb7fdae
John Smith <willey@selby.com>
2016-08-08 16:25:33 -0700

    lots of goofy return chode stuff

--- bin/build_and_push.pl
+++ bin/build_and_push.pl
@@ -1,7 +1,29 @@
 #!/usr/bin/perl
 
-#time /usr/local/cam/bin/build_time_lapse.pl 
-my $ret = `trickle -s -u 200 /usr/local/cam/bin/build_time_lapse.pl $@`;
-if ( $? != 0 ) {
-    print $ret;
+use strict;
+use Getopt::Long;
+
+my $aaargs = join(" ", @ARGV);
+my $debug;
+my $date;
+my $result = GetOptions ( "d|debug+"  => \$debug,
+                          "t|date=s"  => \$date);
+
+if ( $debug ) {
+    print "starting build and parse\n";
 }
+
+my $cmd = "trickle -s -u 200 /usr/local/cam/bin/build_time_lapse.pl $aaargs";
+my $ret = `$cmd`;
+my $exit_code = $?;
+if ( $exit_code != 0 ) {
+    print "exit: ", $exit_code, "\nret: ", $ret, "\n";
+    exit(1);
+}
+if ( $debug ) {
+    print $ret;
+    print "exit code from build_time_lapse.pl: ", $exit_code, "\n";
+    #print $ret;
+    print "ending build and parse\n";
+}
+exit($exit_code);
