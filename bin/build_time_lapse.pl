8a21f37717d094da593cd0b08e32604d99e34486
John Smith <willey@selby.com>
2017-06-04 14:32:26 -0700

    command line arg for fps, fix running commands twice (OMG), clean-up some debugging

--- bin/build_time_lapse.pl
+++ bin/build_time_lapse.pl
@@ -33,6 +33,7 @@ my $trickle_cmd = "trickle -s -u 200";
 my $keep = 0;
 my $out = "";
 my $directory;   # overrides the date
+my $fps;   # overrides fps
 
 my $result = GetOptions (  "n|dry-run" => \$dryrun,
                         "f|force"  => \$force,
@@ -43,6 +44,7 @@ my $result = GetOptions (  "n|dry-run" => \$dryrun,
                         "D|directory=s"  => \$directory,
                         "np|no-push"  => \$no_push,
                         "trickle"  => \$trickle,
+                        "fps=i"  => \$fps,
                         "silent"  => \$silent,
                         "d|debug+"  => \$debug);
 
@@ -86,20 +88,20 @@ else {
 my $s = new SC6::Cam::Sun();
 print "Now: $dt\n" if ( $debug );
 if ( $directory ) {
-    print scalar localtime(), "We hav a direcotry to process: $directory\n";
+    print scalar localtime(), " We hav a direcotry to process: $directory\n";
 }
 elsif ( $force ) {
-    print scalar localtime(), "We're forced to do this\n" if ( $debug );
+    print scalar localtime(), " We're forced to do this\n" if ( $debug );
 }
 else {
     if ( $s->is_sun($dt) ) {
-        print scalar localtime(), "Sun is up!\n" unless ( $silent);
+        print scalar localtime(), " Sun is up!\n" unless ( $silent);
     }
     elsif ( $s->is_hour_after_dusk($dt) ) {
-        print scalar localtime(), "Sun is down, but for less than an hour!\n" unless ( $silent);
+        print scalar localtime(), " Sun is down, but for less than an hour!\n" unless ( $silent);
     }
     else  {
-        print scalar localtime(), "Sun is down!\n" unless ( $silent);
+        print scalar localtime(), " Sun is down!\n" unless ( $silent);
         exit;
     }
 }
@@ -119,8 +121,8 @@ if ( ! $no_push ) {
 cleanup($format, $mode);
 
 if ( $debug ) {
-    print scalar localtime(), "buh bye\n";
-    print scalar localtime(), "push still returned $push_return_code\n";
+    print scalar localtime(), " buh bye\n";
+    print scalar localtime(), " push still returned $push_return_code\n";
 }
 if ( $push_return_code != 0 ) {
     exit 1;
@@ -140,11 +142,15 @@ sub make_moovie {
         $image_dir = get_image_dir($dt, $image_dir, $mode);
     }
 
+    if ( ! $fps ) {
+        $fps = $config->{'FPS'};
+    }
+
     my $in = "'mf://" . $image_dir . "/*_" . $format . ".jpg'";
     my $mf = "w=" . $config->{'Sizes'}->{$format}->{'width'} . 
         ":h=" . $config->{'Sizes'}->{$format}->{'height'} . 
         ":type=" . $config->{'Type'} . 
-        ":fps=" . $config->{'FPS'};
+        ":fps=" . $fps;
 
     my $cmd = "mencoder -msglevel all=1 -nosound -noskip -oac copy -ovc copy -o $out -mf $mf $in";
     my_do_cmd($cmd, $dryrun);
@@ -175,6 +181,7 @@ sub usage
     print "\t-k|--keep      - don't remove disposable video files\n";
     print "\t-np|--no-push  - don't push to youtube\n";
     print "\t--trickle      - use trickle to limit bandwidth\n";
+    print "\t--fps          - override frame per second\n";
     print "\t--silent       - don't print normal amount of information\n";
     print "\t--debug        - print extra debugging information (debug overrides silent)\n";
     print "\t-m|--mode      - mode, prod or test\n";
@@ -226,14 +233,13 @@ sub collect_metadata {
 sub my_do_cmd {
     my ($cmd, $dryrun) = @_;
     if ( $debug ) {
-        print scalar localtime(), $cmd, "\n";
+        print scalar localtime(), " ", $cmd, "\n";
     }
     if ( ! $dryrun ) {
-        print `$cmd` if ( $debug );
+        print scalar localtime(), " Start command\n" if ( $debug );
         my  $ret = `$cmd 2>&1`;
-        print scalar localtime(), "Start command\n" if ( $debug );
         print $ret if ( $debug );
-        print scalar localtime(), "Stop command\n" if ( $debug );
+        print scalar localtime(), " Stop command\n" if ( $debug );
         return $?;
     }
 }
