dede2f1d513becf1f66d18da8548d85b6888e4eb
steve mats mats <steve@matsmats.com>
2023-08-27 14:31:39 -0700

    more debug and fixing some bucket names

--- bin/test_bucket_shiz.pl
+++ bin/test_bucket_shiz.pl
@@ -29,7 +29,7 @@ print "\n***** STARTING *****\n" if ( $debug );
 
 
 # prime bluecode
-my $push_to_google = $config->{'GStore'}->{'Enable'};
+my $push_to_google = $config->{'BucketShiz'}->{'Enable'};
 my $bcr = new SC6::Cam::BlueCodeState($mode, $dryrun);
 
 print "primed bluecode is: ", $bcr->getBluecode(), "\n" if ( $debug );
@@ -49,32 +49,26 @@ my $dt = DateTime->now(  time_zone => $config->{'General'}->{'Timezone'} );
 
 my $current_image = new SC6::Cam::Image($dt, $mode, $dryrun, $s);
 
-my $current = $config->{GStore}->{'CurrentDir'};
 my $www_dir = get_www_dir($format, $mode);
 my $www_image_orig = $www_dir . $config->{Image}->{File}->{orig};
 my $www_image_50pct = $www_dir . $config->{Image}->{File}->{'50pct'};
-my $public = $config->{GStore}->{'PublicDir'};
 my $public_image_orig = $current_image->getPublicWWWFile();
 my $public_image_50pct = $current_image->getPublicWWWFile_50pct();
-my $blue_bucket = $main::config->{GStore}->{'BlueistDir'};
 my $blueist_file_50pct = get_www_dir("", $main::mode) . $main::config->{BlueCode}->{'BlueistImage'} . "_50pct";
 my $blueist_file_orig = get_www_dir("", $main::mode) . $main::config->{BlueCode}->{'BlueistImage'} . "_orig";
 
-# cp blueist to current
-my $current_dir = $config->{GStore}->{'CurrentDir'};
-my $blueist_dir = $config->{GStore}->{'BlueistDir'};
-my $public_dir = $config->{GStore}->{'PublicDir'};
+# bucket dirs
+my $current_dir = $config->{'BucketShiz'}->{'CurrentDir'};
+my $blueist_dir = $config->{'BucketShiz'}->{'BlueistDir'};
+my $public_dir = $config->{'BucketShiz'}->{'PublicDir'};
 my $blueist_image_orig = $blueist_dir . "/" . $config->{Image}->{File}->{orig};
 my $blueist_image_50pct = $blueist_dir . "/" . $config->{Image}->{File}->{'50pct'};
 
-print ("current:\t\t$current\n");
 print ("www_dir:\t\t$www_dir\n");
 print ("www_image_orig:\t\t$www_image_orig\n");
 print ("www_image_50pct:\t$www_image_50pct\n");
-print ("public:\t\t\t$public\n");
 print ("public_image_orig:\t$public_image_orig\n");
 print ("public_image_50pct:\t$public_image_50pct\n");
-print ("blue_bucket:\t\t$blue_bucket\n");
 print ("blueist_file_50pct:\t$blueist_file_50pct\n");
 print ("blueist_file_orig:\t$blueist_file_orig\n");
 
@@ -93,10 +87,11 @@ $gstore->cp_fs2bucket($public_image_50pct, $blue_bucket);
 print("\n");
 $gstore->cp_fs2bucket($public_image_orig, $blue_bucket);
 print("\n");
-$gstore->cp_fs2bucket($public_image_orig, $public);
+$gstore->cp_fs2bucket($public_image_orig, $public_dir);
 print("\n");
-$gstore->cp_fs2bucket($www_image_orig, $current);
-my $full = $current . "/" . basename($www_image_orig);
+$gstore->cp_fs2bucket($www_image_orig, $currect_dir);
+
+my $full = $currect_dir . "/" . basename($www_image_orig);
 my $cmd = "/usr/local/cam/bin/bucket_shiz.py --get_metadata --blob_name $full";
 print("Going to do: ", $cmd, "\n");
 print(`$cmd`);
